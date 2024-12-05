7ecdcc5a7b8926f78726cb29690fb22f3a8ebbf3
nineey <56111891+nineey@users.noreply.github.com>
2024-12-05 14:54:42 +0100

    add store for date selection and add grid

---
+++ composables/store.ts
@@ -0,0 +1,5 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+    selectedDate: new Date()
+})
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,11 @@
 <script setup>
 import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
+import {store} from "~/composables/store.ts";
 
 const user = useSupabaseUser();
 const checkIn = ref(false)
 const checkOut =ref(false)
-const timeStamp = ref([]);
-const date = ref();
 const timeIn = ref({
   hours: new Date().getHours(),
   minutes: new Date().getMinutes()
@@ -32,7 +31,8 @@ const writeTimeStamp = () => {
 
 onMounted(() => {
   const today = new Date();
-  date.value = today;
+  store.selectedDate.value = today;
+  console.log(store.selectedDate.value)
 
   timeIn.value.hours = today.getHours()
   timeIn.value.minutes = today.getMinutes()
@@ -41,23 +41,22 @@ onMounted(() => {
 </script>
 
 <template>
+
   <div class="content-container">
     <p class="mb-3">Aktueller User: {{user.email}}</p>
-    <h1 class="font-bold mb-5">Zeiterfassung</h1>
-    <button :disabled="checkOut" type="button" @click="writeTimeStamp()" class="text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-lg p-5 py-2.5 me-2 mb-2 disabled:bg-slate-300">{{checkIn ? "Check-Out" : "Check-In"}}</button>
-    <p>Ausgewählter Tag: {{ date }}</p>
-    <div class="mt-5 justify-between flex flex-row justify-between	">
-      Ankunft:
-      <VueDatePicker class="basis-1/4" v-model="timeIn" time-picker text-input/>
 
-      Abfahrt:
-      <VueDatePicker class="basis-1/4" v-model="timeOut" time-picker text-input/>
-      <VueDatePicker class="basis-1/4" v-model="date" inline auto-apply />
+
+
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6 h-48">
+      <DailyTimer></DailyTimer>
+      <div class="bg-[#D9B99B] rounded-xl p-6">01</div>
+      <div class="bg-[#D9B99B] rounded-xl p-6">
+        <VueDatePicker class="basis-1/4" v-model="store.selectedDate" inline auto-apply />
+      </div>
     </div>
-    <ul>
-      <li v-for="stamp in timeStamp">
-        {{ stamp}}
-      </li>
-    </ul>
+
   </div>
+
+
 </template>
